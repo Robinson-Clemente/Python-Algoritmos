e1004d55de3e06e97dca5d9508ece97f12dc7f27
Robinson Clemente <robinsondaniel38@hotmail.com>
2023-06-10 12:00:34 -0500

    se corrige bug en el método encontrarProximoMayorIgual

--- Algoritmo_Ordenamiento.py
+++ Algoritmo_Ordenamiento.py
@@ -3,10 +3,10 @@
 # Se realiza algoritmo de ordenación ascendente de forma recursiva
 
 
-#lista = [16, 15, 13, 12, 11, 19, 38, 37, 36,
-        #35, 34, 33, 32, 8, 7, 6, 5, 4, 3, 2, 1]
+lista = [16, 15, 13, 12, 11, 19, 38, 37, 36,
+        35, 34, 33, 32, 8, 7, 6, 5, 4, 3, 2, 1]
          
-lista = [8,6,4,1,7,5,3,2,1]
+#lista = [8,6,4,1,7,5,3,2,1]
 #lista = [1,2,1,2,1,2,1,2,1,2,1,2]
 sublistas_clasificadas = []
 
@@ -47,8 +47,7 @@ def dividirLista(lista):
     return temporal_list
 
 def ordenarAscendentemente(sublista):
-    if len(sublista) == 4:
-        
+    if len(sublista) == 4:        
         sublista = ordenarPares(sublista)   
         
         """
@@ -69,27 +68,13 @@ def ordenarAscendentemente(sublista):
         return sublista
 
     elif len(sublista) == 3:
-
-        #ordenarPar(0, 1, sublista)
-
-        # Procedemos a comparar con el extremo único
-        # Primer elemento del par
         """
-        if not (sublista[0] < sublista[2]):
-            temporal = sublista[2]
-            sublista.remove(temporal)
-            sublista.insert(0, temporal)
-
-        # Segundo elemento del primer par
-        if not (sublista[1] < sublista[2]):
-            temporal = sublista[2]
-            sublista.remove(temporal)
-            sublista.insert(1, temporal)
-        """
-        
-        sublista = ordenarPar(0, 1, sublista)   
-        sublista = ordenarPar(0, 2, sublista)   
-        return ordenarPar(1, 2, sublista)
+        Se ordena el primer par y luego se compara cada elemento de dicho par con el
+        el último elemento.
+        """                
+        sublista = ordenarParEspecial(0, 1, sublista)   
+        sublista = ordenarParEspecial(0, 2, sublista)   
+        return ordenarParEspecial(1, 2, sublista)
 
     elif len(sublista) == 2:
         return ordenarPar(0, 1, sublista)
@@ -214,8 +199,7 @@ def insercionIntermediaRecursiva(indice, lista0, lista1):
                         indice = lista0.index(numero)
                         insercionIntermediaRecursiva(indice, lista0, lista1)
 
-def encontrarUltimoNumeroIndiceRepetido(contador, numero_indice, sublista0):
-    
+def encontrarUltimoNumeroIndiceRepetido(contador, numero_indice, sublista0):    
     numero = 0
    
     indice = (len(sublista0) - 1) - contador
@@ -232,19 +216,17 @@ def encontrarUltimoNumeroIndiceRepetido(contador, numero_indice, sublista0):
         return encontrarUltimoNumeroIndiceRepetido(contador, numero_indice, sublista0)
 
 # Este método busca el primer número mayor de la lista de izquierda a derecha
-def encontrarProximoMayorIgual(indice, numero, sublista0):
-    
+def encontrarProximoMayorIgual(indice, numero, sublista0):    
     if numero <= sublista0[indice]:
         return indice
-    else:
-        indice += 1
-        if indice < len(sublista0):
+    else:        
+        if (indice+1) < len(sublista0):
+            indice += 1
             return encontrarProximoMayorIgual(indice, numero, sublista0)
         else:
             return indice
         
 def insercionIntermediaRecursivaMejorada(indice, numero, sublista0, sublista1):
-
     if indice < (len(sublista0) - 1) and len(sublista1) >= 1:
         """ Corresponde tratar el caso cuando la lista0 tiene menos elementos que la lista1
         y ya se llegó al final """
@@ -381,8 +363,6 @@ def evaluarExtremosEInsertar(sublista0, sublista1):
         insercionIntermediaRecursiva(0, sublista0, sublista1)
     """
     
-    
-    
 def fusionarSublistas(finalistas):    
     if len(finalistas) > 1:        
         lista = evaluarExtremosEInsertar(finalistas.pop(0), finalistas.pop(0))
@@ -390,7 +370,6 @@ def fusionarSublistas(finalistas):
         fusionarSublistas(finalistas)
 
 def generarSublistas(lista):
-
     sublistas = dividirLista(lista)
     sublista_izq = sublistas[0]
     sublista_der = sublistas[1]        
@@ -400,23 +379,13 @@ def generarSublistas(lista):
     if sublista_izq is not None:
         sublistas_clasificadas.append(sublista_izq)
         print("Sublista izq: ", sublista_izq)        
-
+    
     sublista_der = ordenarAscendentemente(sublista_der)
 
     if sublista_der is not None:
         sublistas_clasificadas.append(sublista_der)
         print("Sublista der: ", sublista_der)
     
-    """
-        Es posible que aquí se añada la lógica para que al acabarse las llamadas
-        entonces se ejecute la lógica para organizar los finalistas, así evito
-        recorrer el "arbol" dos veces. Es decir, si cuando el proceso recursivo
-        termina este empieza a devolverse de donde terminó, ¿por qué no aprove-
-        char esa oportunidad para ser más eficiente? ¿O es más rápido utilizar 
-        los hilos con la lista de sublistas clasificadas? ¿o por qué no utili-
-        zar los hilos al menos en la división principal de la lista inicial?
-    """
-
 print("|------------- ANTES ----------------|")
 print(lista)
 print("|------------- FINALISTAS ----------------|")
