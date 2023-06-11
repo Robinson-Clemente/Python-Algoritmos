d0f1089ac309e7dc91d9b09353184419884ebff5
Robinson Clemente <robinsondaniel38@hotmail.com>
2023-06-11 18:38:56 -0500

    Algoritmo_Número_Mayor.py: se corrige bug en el segundo método y se renombran variables y métodos

--- Algoritmo_Número_Mayor.py
+++ Algoritmo_Número_Mayor.py
@@ -7,9 +7,9 @@ Notas: - Este código necesita ser mejorado. Solo basta el método desarrollado
 para recorrer tanto por izquierda como por derecha.
 """
 
-lista = [-100,21,23,24,101,6,7,8,9,71,17]
+lista = [-100,21,23,24,101,6,7,8,9,71,17,500]
 
-def clasificarMayorOld(lista):   
+def clasificarMayorFormaUno(lista):   
     # Se procede a dividir la lista a la mitad entera
     mitad = 0
     
@@ -25,67 +25,68 @@ def clasificarMayorOld(lista):
     # Pasamos el resultado float a integer 
     mitad = int(mitad)       
     # Sublista para la primera mitad
-    sub_lista_izq = lista[:mitad]
+    sublista_izq = lista[:mitad]
     # Sublista para la segunda mitad
-    sub_lista_der = lista[mitad:]
+    sublista_der = lista[mitad:]
 
     #Se profundiza por izquierda
     mayor_izq = 0  
     
     #como se llego a length igual a 2 significa que es momento de comparar        
-    if len(sub_lista_izq) == 2:        
-        if sub_lista_izq[0] > sub_lista_izq[1]:            
-            mayor_izq = sub_lista_izq[0]            
+    if len(sublista_izq) == 2:        
+        if sublista_izq[0] > sublista_izq[1]:            
+            mayor_izq = sublista_izq[0]            
         else:            
-            mayor_izq = sub_lista_izq[1] 
+            mayor_izq = sublista_izq[1] 
         """
         se trata el caso cuando la división genera una sublista de tamaño impar menor a 3,
         es decir cuando el tamaño de la sublista es igual a 1.
         """
                
-    elif len(sub_lista_izq) == 1:
-        if sub_lista_izq[0] > mayor_izq:            
-            mayor_izq = sub_lista_izq[0]
+    elif len(sublista_izq) == 1:
+        if sublista_izq[0] > mayor_izq:            
+            mayor_izq = sublista_izq[0]
         """
         Como el length no es igual a 1 ni a 2, entonces
         es necesario seguir diviendo la sublista
         """    
     else:                
-        mayor_izq = clasificarMayorOld(sub_lista_izq)
+        mayor_izq = clasificarMayorFormaUno(sublista_izq)
                 
     #Se profundiza por derecha
     mayor_der = 0  
     
-    if len(sub_lista_der) == 2:        
-        if sub_lista_der[0]>sub_lista_der[1]:                        
-            mayor_der = sub_lista_der[0]            
+    if len(sublista_der) == 2:        
+        if sublista_der[0]>sublista_der[1]:                        
+            mayor_der = sublista_der[0]            
         else:            
-            mayor_der = sub_lista_der[1]               
-    elif len(sub_lista_der) == 1:
-        if sub_lista_der[0] > mayor_der:            
-            mayor_der = sub_lista_der[0]     
+            mayor_der = sublista_der[1]               
+    elif len(sublista_der) == 1:
+        if sublista_der[0] > mayor_der:            
+            mayor_der = sublista_der[0]     
     else:
-        mayor_der = clasificarMayorOld(sub_lista_der)    
+        mayor_der = clasificarMayorFormaUno(sublista_der)        
+            
     if mayor_izq > mayor_der:                
         return mayor_izq
     else:                
         return mayor_der
     
-def encontrarMayor(indice, mayor, sublista0):    
-    if (indice + 1) < len(sublista0):
+def encontrarMayorFormaDos(indice, mayor, sublista0):
+    if indice < len(sublista0):
         if sublista0[indice] > mayor:
             mayor = sublista0[indice]
             indice += 1
-            mayor = encontrarMayor(indice, mayor, sublista0)
+            mayor = encontrarMayorFormaDos(indice, mayor, sublista0)
         else:
             indice += 1
-            mayor = encontrarMayor(indice, mayor, sublista0)
+            mayor = encontrarMayorFormaDos(indice, mayor, sublista0)
     return mayor
 
 print("|-------------------- PRIMER MÉTODO --------------------|")
-resultado = clasificarMayorOld(lista)
+resultado = clasificarMayorFormaUno(lista)
 print("El número clasificado es:", resultado)
 print("|-------------------- SEGUNDO MÉTODO --------------------|")
-resultado = encontrarMayor(0, 0, lista)
+resultado = encontrarMayorFormaDos(0, 0, lista)
 print("El número clasificado es:", resultado)
 
